149995b9dd21bb0f01eb0c8e4b12f77405997ecd
Aaron Glenn <aaron@aaron-glenns-macbook-pro.local>
2009-06-12 01:07:17 -0600

    Some nav helpers.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,28 @@ module ApplicationHelper
   def admin?
     true
   end
+  
+  def site_nav
+    if @page
+      html = '<ul>'
+      @page.children.each do |page|
+        html << "<li>#{link_to page.title, page_path(page.id)}"
+        html << nav_for(page) if page.children
+        html << "</li>"
+      end
+      html << '</ul>'
+    else
+      
+    end
+  end
+  
+  def nav_for(page)
+    html = '<ul>'
+    page.children.each do |page|
+      html << "<li>#{link_to page.title, page_path(page.id)}</li>"
+    end
+    html << '</ul>'
+  end
+  
+  
 end
