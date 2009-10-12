8afd458bb9f0ba4f98723a4a1be10a7d8db5ae9d
Aaron Glenn <aaron@abookpro.local>
2009-10-11 21:46:56 -0600

    Draggable content

--- public/javascripts/stockyard.js
+++ public/javascripts/stockyard.js
@@ -2,7 +2,14 @@ var Stockyard = Class.create();
 Stockyard.prototype = {
   initialize : function(){
     this.setup_new_content_form();
+    this.setup_image_previews();
   }, 
+  
+  setup_image_previews: function(){
+    $$('.basic_image_preview').each(function(preview) {
+      preview.observe('hover', function(p){p.toggleClassName('hover')});
+    });
+  },
   
   setup_new_content_form: function(){
     content_type_select = $('content_mapping_asset_type_selector');
