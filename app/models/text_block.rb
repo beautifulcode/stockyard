552a2315bf4b68c8a18196d204b29a39cda76316
Aaron Glenn <aaron@abookpro.local>
2009-10-26 01:25:29 -0600

    Not ready.

--- app/models/text_block.rb
+++ app/models/text_block.rb
@@ -1,6 +1,6 @@
 class TextBlock < Asset
   
-  belongs_to :nested_image, :class => "BasicImage", :foreign_key => :nested_image_id
+  # belongs_to :nested_image, :class => "BasicImage", :foreign_key => :nested_image_id
   
   translates :content
   accepts_nested_attributes_for :globalize_translations 
